d66d0b8a7350251ae78d6509011b3826b7212647
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-10-04 18:23:55 +0300

    Routing

--- src/components/FinishedQuiz/FinishedQuiz.js
+++ src/components/FinishedQuiz/FinishedQuiz.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import classes from './FinishedQuiz.module.css';
 import Button from '../UI/Button/Button';
+import { Link } from 'react-router-dom'
 
 
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
-        if (props.results[key]==='success') {
+        if (props.results[key] === 'success') {
             total++
         }
         return total
@@ -13,20 +14,20 @@ const FinishedQuiz = props => {
     return (
         <div className={classes.FinishedQuiz}>
             <ul>
-                { props.quiz.map((quizItem, index) => {
+                {props.quiz.map((quizItem, index) => {
                     const cls = [
                         'fa',
-                        props.results[quizItem.id] === 'error'? 'fa-times': 'fa-check',
+                        props.results[quizItem.id] === 'error' ? 'fa-times' : 'fa-check',
                         classes[props.results[quizItem.id]]
                     ]
                     return (
                         <li key={index}>
-                            <strong>{ index + 1 }</strong>. &nbsp;
+                            <strong>{index + 1}</strong>. &nbsp;
                             {quizItem.question}
                             <i className={cls.join(' ')} />
                         </li>
                     )
-                }) }
+                })}
                 {/*<li>
                     <strong>1. </strong>
                     How are you?
@@ -41,7 +42,10 @@ const FinishedQuiz = props => {
             <p>Правильно {successCount} из {props.quiz.length}</p>
             <div>
                 <Button onClick={props.onRetry} type="primary">Повторить</Button>
-                <Button type="success">Перейти в список тестов</Button>
+                <Link to="/">
+                    <Button type="success">Перейти в список тестов</Button>
+                </Link>
+
             </div>
         </div>
     )
--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,32 +1,45 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import classes from './Drawer.module.css';
+import {NavLink} from 'react-router-dom';
 import Backdrop from "../../UI/Backdrop/Backdrop";
 
-    const links = [
-        1, 2, 3
-    ]
+const links = [
+    { to: '/', label: 'Список', exact: true },
+    { to: '/auth', label: 'Авторизация', exact: false },
+    { to: '/quiz-creator', label: 'Создать тест', exact: false },
+]
 
 class Drawer extends Component {
+
+    clickHandler = ()=> {
+        this.props.onClose();
+    }
+
     renderLinks() {
         return links.map((link, index) => {
             return (
                 <li key={index}>
-                    <a>Link {link}</a>
+                    <NavLink 
+                            to={link.to} 
+                            exact={link.exact}
+                            activeClassName={classes.active}
+                            onClick={this.clickHandler}>
+                        {link.label}
+                    </NavLink>
                 </li>
             )
         })
     }
     render() {
         const cls = [classes.Drawer]
-
         if (!this.props.isOpen) {
             cls.push(classes.close)
         }
-        return(
+        return (
             <React.Fragment>
                 <nav className={cls.join(' ')}>
                     <ul>
-                        { this.renderLinks()}
+                        {this.renderLinks()}
                     </ul>
                 </nav>
                 {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
